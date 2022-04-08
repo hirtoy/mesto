fc481920945275a8bdb6382bdfa01c6062f80e04
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-04-08 19:32:16 +0300

    Правки

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ let btnClosePopup = document.querySelector('.popup__close');
 let profileEditForm = document.forms['profileEdit'];
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
+const formElement = document.querySelector('.popup__container');
 
 let profileNameInput = document.querySelector('.popup__input_type_name');
 let profileJobInput = document.querySelector('.popup__input_type_job');
